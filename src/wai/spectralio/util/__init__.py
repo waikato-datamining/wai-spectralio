e548f59da23307214e3fb8a479f88dfaaedff3d9
csterling <coreytsterling@gmail.com>
2020-01-30 14:48:15 +1300

    Added utility decorators.

--- src/wai/spectralio/util/__init__.py
+++ src/wai/spectralio/util/__init__.py
@@ -1,1 +1,3 @@
+from ._dynamic_default import dynamic_default
 from ._instanceoptionalmethod import instanceoptionalmethod
+from ._with_locale import with_locale
---
+++ src/wai/spectralio/util/_with_locale.py
@@ -0,0 +1,30 @@
+import functools
+import locale
+
+
+def with_locale(locale_to_use: str, category: int = locale.LC_NUMERIC):
+    """
+    Function which creates a decorator which sets the locale before
+    the wrapped function is called, and reverts it after it is finished.
+
+    :param locale_to_use:   The locale to use.
+    :param category:        The locale category to modify.
+    :return:                The decorator.
+    """
+    def decorator(function):
+        @functools.wraps(function)
+        def wrapper(*args, **kwargs):
+            # Save the current locale
+            saved_locale = locale.getlocale(category)
+
+            # Set the locale
+            locale.setlocale(category, locale_to_use)
+
+            try:
+                return function(*args, **kwargs)
+            finally:
+                locale.setlocale(category, saved_locale)
+
+        return wrapper
+
+    return decorator
